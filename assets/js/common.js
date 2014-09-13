005b612c070a7f35a7c28eb916e9d02b8c50aaca
yecol <xujb1987@gmail.com>
2014-09-13 10:22:55 +0100

    finished header and footer.

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,6 +3,8 @@ var offsets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -60];
 $(document).ready(function(){
 
     //menu
+    $fixedLeft = $("#nav-1 li.active a").position().left;
+    $("ul[class='menu-2 active']").css('left', $fixedLeft);
     $('#nav-1 a').hover(function(){
         $(this).parent().siblings().removeClass("on");
         $(this).parent().addClass("on");
@@ -14,6 +16,12 @@ $(document).ready(function(){
         $('.menu-2').eq($curIndex).css({
             left: $targetLeft + offsets[$curIndex] + 'px'
         });
+        $("ul[class='menu-2 active']").css('left', '-9999px');
     });
+    $("section#nav").mouseleave(function(){
+        console.log("out-1");
+        $('.menu-2').css({left: '-9999px'});
+        $("ul[class='menu-2 active']").css('left', $fixedLeft);
+    })
 
 });
