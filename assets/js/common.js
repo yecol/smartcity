8362a65b40692f93e4f97f6b0c22be70fd73e290
yecol <xujb1987@gmail.com>
2014-09-14 22:03:25 +0100

    index page finished.

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,13 +2,35 @@ var offsets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -60];
 
 $(document).ready(function(){
 
+    // carousel
+    $('.carousel').unslider({
+        dots: true
+    });
+    var unslider = $('.carousel').unslider();
+    
+    $('.unslider-arrow').click(function(e) {
+        e.preventDefault();
+        var fn = this.className.split(' ')[1];
+        
+        //  Either do unslider.data('unslider').next() or .prev() depending on the className
+        unslider.data('unslider')[fn]();
+    });
+
+    $(".dots").css('left', ($(".carousel").width()-$(".dots").width())/2);
+
+    // tab
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
+
     //menu
-    $fixedLeft = $("#nav-1 li.active a").position().left;
+    $fixedLeft = $("#nav-1 li.active a").position().left + 10;
     $("ul[class='menu-2 active']").css('left', $fixedLeft);
     $('#nav-1 a').hover(function(){
         $(this).parent().siblings().removeClass("on");
         $(this).parent().addClass("on");
-        $targetLeft = $(this).position().left;
+        $targetLeft = $(this).position().left + 10;
         $curIndex = $('#nav-1 li').index($(this).parent());
         $('.menu-2').css({
             left: '-9999px'
